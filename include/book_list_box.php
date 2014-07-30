7fd8d37322d0649e495e3d59118d93fb8e2a10d3
ppliuppliu <ppliuppliu@163.com>
2014-07-30 22:08:12 +0800

    new

--- include/book_list_box.php
+++
@@ -1,8 +0,0 @@
-<li><a href="book.php" class="book_list_box">
-    <img data-src="holder.js/50x60" class="pull-left" alt="">
-    <div class="pull-left slide_book_list_content">
-        <p class="slide_book_list_content_name">破产姐妹<span class="slide_book_list_content_time">3天前发布</span></p>
-        <p>交大东区--自己联系</p>
-        <p class="slide_book_list_content_price">￥300</p>
-    </div>
-</a></li>
